b7f8373d134f21d7acba42afe6e9a3b2be15e797
Christos Constantinou <snez@snez.org>
2013-09-06 16:23:00 +0300

    template for tweets

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,10 +1,24 @@
 class SearchController < ApplicationController
 
 def index
-	@tweets = "tweet 1", "tweet 2"
+	@tweets = []
 	@links = "link 1", "link 2"
 	@videos = "video 1", "video 2"
 	@photos = "photo 1", "photo 2"
+
+	tweet = JSON.parse('
+{
+	"id" : 1,
+	"username" : "christosconst",
+	"name" : "Christos Constantinou",
+	"avatar" : "https://si0.twimg.com/profile_images/1571459461/sketch_normal.jpg",
+	"content" : "The actual tweet"
+}
+	')
+
+	@tweets << tweet
+	@tweets << tweet
+	@tweets << tweet
 end
 
 end
