451bab44ce1744a0a968853647a9ab10d3e0788a
Christos Constantinou <snez@snez.org>
2013-09-06 15:51:23 +0300

    the 4 types of content

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,10 @@
 class SearchController < ApplicationController
 
 def index
-
+	@tweets = "tweet 1", "tweet 2"
+	@links = "link 1", "link 2"
+	@videos = "video 1", "video 2"
+	@photos = "photo 1", "photo 2"
 end
 
 end
