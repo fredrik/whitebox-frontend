78860dc7d90cd1487fd100f9fe54654a10f533e2
Christos Constantinou <snez@snez.org>
2013-09-07 12:04:13 +0300

    Set the twitter handle

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -5,6 +5,15 @@ def index
 		cookies[:fb] = params["fb"]
 		redirect_to root_path
 	end
+
+	if params["screen_name"].present?
+		if params["screen_name"] == "0"
+			cookies.delete(:twitter)
+		else
+			cookies[:twitter] = params["screen_name"]
+		end
+		redirect_to root_path
+	end
 end
 
 end
