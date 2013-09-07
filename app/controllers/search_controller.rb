0a56dc0ccfcfee9833c39db0091f2f0114e358ea
Christos Constantinou <snez@snez.org>
2013-09-07 08:56:51 +0300

    Save facebook auth cookies

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,17 +1,10 @@
 class SearchController < ApplicationController
 
 def index
-	@tweets = []
-	@links = "link 1", "link 2"
-	@videos = "video 1", "video 2"
-	@photos = "photo 1", "photo 2"
-
-	file = File.open("tweet.json", "rb")
-	tweet = JSON.parse(file.read)
-
-	@tweets << tweet
-	@tweets << tweet
-	@tweets << tweet
+	if (not cookies[:fb].present?) && params["fb"].present?
+		cookies[:fb] = params["fb"]
+		redirect_to root_path
+	end
 end
 
 end
